eca9d64d87462a5b1930acecfd3bf122bbdf0fbb
Peter Cai <xqsx43cxy@126.com>
2013-07-14 13:59:37 +0800

    增加wp_footer支持
    这样可以修复对一些插件的支持

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer id="footer">
+<?php wp_footer(); ?>
     <div class="w">
         <a href="http://www.wordpress.org/" target="_blank" title="Wordpress强力驱动" class="wordpress"></a>
         &copy; 2011 - 2012 youed.me / Theme by <a href="http://youed.me/" title="yoUED/yoTheme">Ethan</a>
